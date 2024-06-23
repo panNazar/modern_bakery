d2d8babc1cc2691cdc9be0e2eed3b10dd64b0c1d
panNazar <125811447+panNazar@users.noreply.github.com>
2024-06-23 15:44:35 +0300

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector('.backdrop');
+const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
+const modalBtnClose = document.querySelector('.modal-btn-close');
+
+const toggleModal = () => modal.classList.toggle('is-hidden');
+
+    modalBtnOpen.forEach(btn => {
+        btn.addEventListener('click', toggleModal);
+    });
+modalBtnClose.addEventListener('click', toggleModal);
+    
+
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
